93fe951b37d46ad80ba646c4ea3812b257911c98
Peter Hanson <peterhanson@email.arizona.edu>
2018-05-03 23:28:19 -0700

    Major progress!
    Made front end website prototype, about half way done with server side
    java logic, and finished the redis data base integration and data
    structure design.

--- src/main/java/com/peterhanson/HomePage.java
+++ src/main/java/com/peterhanson/HomePage.java
@@ -2,17 +2,160 @@ package com.peterhanson;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.protocol.http.WebApplication;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+	//choices in dropdown box
+	public static List<String> contactNames = new ArrayList<String>();
 
+	
+	
+	String name;
+	String address;
+	String phone;
+	String email;
+	String relationship;
+	
+	
+	
+	//variable to hold the selected value from dropdown box,
+	//and also make "Create new contact" is selected by default
+	private String selected = "Create new contact";
+	
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+		//add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
 		// TODO Add your page's components here
+		
+		this.updateContactNames();
+		
+		
+//		DropDownChoice<String> listSites = new DropDownChoice<String>(
+//				"contactDropDown", new PropertyModel<String>(this, "selected"), contactNames);
+//		add(listSites);
+		
+		
+	
+		Form<?> form = new ContactForm("form");
+		add(form);
+		
+		
+		//testing hard coded values
+		this.name = "john doe";
+		this.address = "123 Pleasant Lane";
+		this.phone = "3928439238";
+		this.email = "john@email.com";
+		this.relationship = "valued customer";
+		//they appear in the fields
+		
+		
+		//form.add(new TextField<String>("input", new PropertyModel<String>(this, "inputValue")));
 
     }
+	
+	class ContactForm extends Form<Object> {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 9099971868919563238L;
+
+		@SuppressWarnings("unchecked")
+		public ContactForm(String id) {
+			super(id);
+			
+			//TODO: add an event listener on change to the contactDropDown, or if impossible add a button next to it to see what
+			//the current value for it is and then act accordingly
+			add(new DropDownChoice<String>("contactDropDown", new PropertyModel<String>(HomePage.this, "selected"), contactNames));
+			
+//			add(new TextField<String>("Text", new PropertyModel(HomePage.this, "")));
+			add(new TextField<String>("nameText", new PropertyModel<String>(HomePage.this, "name")));
+			add(new TextField<String>("addressText", new PropertyModel<String>(HomePage.this, "address")));
+			add(new TextField<String>("phoneText", new PropertyModel<String>(HomePage.this, "phone")));
+			add(new TextField<String>("emailText", new PropertyModel<String>(HomePage.this, "email")));
+			add(new TextField<String>("relationshipText", new PropertyModel<String>(HomePage.this, "relationship")));
+			
+			//potentially useful code snippet found online about adding an on change event listener
+//			myForm.add(new AjaxEventBehavior("onchange") {
+//			    @Override
+//			    protected void onEvent(AjaxRequestTarget target) {
+//			         handleFormChange(...);
+//			    }
+//			});
+			
+			//TODO: add these buttons to the html with wicket:id links
+//			add(new Button("save") {
+//				/**
+//				 * 
+//				 */
+//				private static final long serialVersionUID = 6387503322049573347L;
+//
+//				@Override
+//				public void onSubmit() {
+//					HomePage.this.saveContact();
+//				}
+//			});
+//			
+//			add(new Button("delete") {
+//				/**
+//				 * 
+//				 */
+//				private static final long serialVersionUID = -7938382276221616082L;
+//
+//				@Override
+//				public void onSubmit() {
+//					HomePage.this.deleteContact();
+//				}
+//			});
+		
+		
+		
+		}
+		
+		
+		
+	}
+	
+	public void updateContactNames() {
+		contactNames = new ArrayList<String>();
+		contactNames.add(selected);
+		for(String s: WicketApplication.getContactKeys()) {
+			contactNames.add(s);
+		}
+	}
+	
+	//TODO: implement saveContact and deleteContact, 
+	
+	
+	
+	
+	//handles case where the save/update button is clicked
+	public void saveContact() {
+		
+	}
+	
+	public void deleteContact() {
+		
+	}
+	
+	
 }
--- src/main/java/com/peterhanson/WicketApplication.java
+++ src/main/java/com/peterhanson/WicketApplication.java
@@ -1,5 +1,10 @@
 package com.peterhanson;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
@@ -13,6 +18,28 @@ import redis.clients.jedis.Jedis;
  */
 public class WicketApplication extends WebApplication
 {
+	
+	
+	//Redis Database Structure
+	//Contact objects are just objects with 5 strings, the name will be the primary key
+	//A set of all primary keys, the name is stored in the String CONTACT_KEYS_SET_NAME
+	//A hashmap, the name is stored in the String CONTACT_HASHMAP_PREFIX, called "contactsMap" + name, with 5 fields: "name", "address", "phone", "email", "relationship"
+			
+	public static final String CONTACT_KEYS_SET_NAME = "contactsKeys";
+	public static final String CONTACT_HASHMAP_PREFIX = "contactsMap";
+	public static final String HASHMAP_FIELDNAME_NAME = "name";
+	public static final String HASHMAP_FIELDNAME_ADDRESS = "address";
+	public static final String HASHMAP_FIELDNAME_PHONE = "phone";
+	public static final String HASHMAP_FIELDNAME_EMAIL = "email";
+	public static final String HASHMAP_FIELDNAME_RELATIONSHIP = "relationship";
+	
+	public static Jedis jedis = new Jedis("localhost");
+	public static Set<String> contactKeys;
+	public static ArrayList<Contact> contacts;
+	
+	
+	
+	
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
@@ -30,19 +57,94 @@ public class WicketApplication extends WebApplication
 	{
 		super.init();
 		
+		this.updateContactKeys();
+		this.populateContacts();
 		
 		
-		Jedis jedis = new Jedis("localhost");
-		//jedis.set("foo", "bar");
-		String value = jedis.get("foo");
 		
-		
-		System.out.println(" ");
-		System.out.println(" ");
-		System.out.println(value);
-		System.out.println(" ");
-		System.out.println(" ");
+//		System.out.println(" ");
+//		System.out.println(" ");
+//		System.out.println(value);
+//		System.out.println(" ");
+//		System.out.println(" ");
 
 		// add your configuration here
 	}
+	
+
+	
+	public static Set<String> getContactKeys(){
+		
+		return contactKeys;
+	}
+	
+	public void updateContactKeys(){
+		
+		this.contactKeys = this.jedis.smembers(CONTACT_KEYS_SET_NAME);
+//		this.populateContacts();
+		
+	}
+	
+	public void addToContactKeys(String newContactName) {
+		this.jedis.sadd(CONTACT_KEYS_SET_NAME, newContactName);
+		this.updateContactKeys();
+	}
+	
+	public void removeFromContactKeys(String oldContactName) {
+		this.jedis.srem(CONTACT_KEYS_SET_NAME, oldContactName);
+		this.updateContactKeys();
+	}
+	
+	public void populateContacts() {
+		
+		contacts = new ArrayList<Contact>();
+		
+		for(String nameKey: contactKeys) {
+			Map<String, String> contactProperties = this.jedis.hgetAll(CONTACT_HASHMAP_PREFIX + nameKey);
+			Contact contact = new Contact();
+			contact.setName(nameKey);
+			contact.setAddress(contactProperties.get("address"));
+			contact.setPhone(contactProperties.get("phone"));
+			contact.setEmail(contactProperties.get("email"));
+			contact.setRelationship(contactProperties.get("relationship"));
+			contacts.add(contact);
+		}
+		
+	}
+	
+	public void addContact(Contact newContact) {
+		
+		
+		this.addToContactKeys(newContact.getName());
+		
+		String contactHashmapKey = CONTACT_HASHMAP_PREFIX + newContact.getName();
+		
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_NAME, newContact.getName());
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_ADDRESS, newContact.getAddress());
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_PHONE, newContact.getPhone());
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_EMAIL, newContact.getEmail());
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_RELATIONSHIP, newContact.getRelationship());
+		contacts.add(newContact);
+		
+	}
+	
+	public void removeContact(String oldContactName) {
+		
+		this.removeFromContactKeys(oldContactName);
+		this.jedis.hdel(CONTACT_HASHMAP_PREFIX + oldContactName);
+		this.populateContacts();
+		
+	}
+	
+	public void editContact(Contact updatedContact) {
+		
+		String contactHashmapKey = CONTACT_HASHMAP_PREFIX + updatedContact.getName();
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_ADDRESS, updatedContact.getAddress());
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_PHONE, updatedContact.getPhone());
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_EMAIL, updatedContact.getEmail());
+		this.jedis.hset(contactHashmapKey, HASHMAP_FIELDNAME_RELATIONSHIP, updatedContact.getRelationship());
+		this.populateContacts();
+	}
+	
+	
 }
